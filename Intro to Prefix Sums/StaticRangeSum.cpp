b8e8dc35568bcb2cb3fdca6a37257814dba2d6a9
Adam <duhaoxuan@gmail.com>
2025-11-29 16:49:34 -0800

    Create Static Range Sum

---
+++ Intro to Prefix Sums/StaticRangeSum.cpp
@@ -0,0 +1,25 @@
+/* 
+Link for problem: https://judge.yosupo.jp/problem/static_range_sum
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    vector<long long> pre(N + 1, 0);
+    for (int i = 0; i < N; i++) {
+        pre[i + 1] = pre[i] + A[i];
+    }
+    for (int i = 0; i < Q; i++) {
+        int l, r;
+        cin >> l >> r;
+        cout << pre[r] - pre[l] << "\n";
+    }
+}
